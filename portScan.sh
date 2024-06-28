8b18e85309f9a99ba10d0921898afcf231246b9a
Joel-C-P <joelcpd77@gmail.com>
2024-06-28 11:23:33 -0400

    EL escaner de puerto ya funciona

--- portScan.sh
+++ portScan.sh
@@ -3,12 +3,15 @@
 
 #Funcion para poder salir de la ejecucion con CTRL+C
 function ctrl_c(){
+
   echo -e "\n\nGetting out...\n"
   exit 1
 
 }
 
-
+pon_tu_ip="127.0.0.1"
 trap ctrl_c INT
 
-sleep 10
+for port in $(seq 1 65535); do
+  (echo "" >/dev/tcp/$pon_tu_ip/$port) 2>/dev/null && echo "[+] EL puerto $port esta abierto"
+done
